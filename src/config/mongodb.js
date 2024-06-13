ba30614dd525a770d06bbe843320a2cb48876b7c
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-14 02:27:30 +0700

    Connect MongoDB With env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,29 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { env } from '~/config/environment';
+
+// Khởi tạo = null vì chưa connect db
+let trelloDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+// Connect Database
+export const CONNECT_DB = async () => {
+    await mongoClientInstance.connect();
+    trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
+};
+
+export const GET_DB = () => {
+    if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!');
+    return trelloDatabaseInstance;
+};
+
+// Close connect Database
+export const CLOSE_DB = async () => {
+    await mongoClientInstance.close();
+};
--- src/server.js
+++ src/server.js
@@ -1,29 +1,32 @@
 import express from 'express';
-import { mapOrder } from '~/utils/sorts.js';
+import exitHook from 'async-exit-hook';
+import { env } from '~/config/environment';
 
-const app = express();
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
 
-const hostname = 'localhost';
-const port = 8017;
+const START_SERVER = () => {
+    const app = express();
 
-app.get('/', (req, res) => {
-    // Test Absolute import mapOrder
-    console.log(
-        mapOrder(
-            [
-                { id: 'id-1', name: 'One' },
-                { id: 'id-2', name: 'Two' },
-                { id: 'id-3', name: 'Three' },
-                { id: 'id-4', name: 'Four' },
-                { id: 'id-5', name: 'Five' },
-            ],
-            ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-            'id',
-        ),
-    );
-    res.end('<h1>Hello World!</h1><hr>');
-});
+    app.get('/', (req, res) => {
+        res.end('<h1>Hello World!</h1><hr>');
+    });
 
-app.listen(port, hostname, () => {
-    console.log(`Hello Tran Phong Dev, I am running at ${hostname}:${port}/`);
-});
+    app.listen(env.APP_PORT, env.APP_HOST, () => {
+        console.log(`3. Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}/`);
+    });
+
+    exitHook(() => CLOSE_DB());
+};
+
+(async () => {
+    try {
+        console.log('1. Connecting to MongoDB Cloud Atlas...');
+        await CONNECT_DB();
+        console.log('2. Connected to MongoDB Cloud Atlas!');
+
+        START_SERVER();
+    } catch (error) {
+        console.error(error);
+        process.exit(0);
+    }
+})();
