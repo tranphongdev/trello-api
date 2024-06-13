c32ab35b26dca0db47afe1beb86b8dc743c72aa1
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-14 03:11:09 +0700

    Validation with Joi

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import { boardValidation } from '~/validations/boardValidation';
+
 const Router = express.Router();
 
 Router.route('/')
     .get((req, res) => {
         res.status(StatusCodes.OK).json({ message: 'GET: APIs v1 get list board.' });
     })
-    .post((req, res) => {
-        res.status(StatusCodes.CREATED).json({ message: 'POST: APIs v1 create new board.' });
-    });
+    .post(boardValidation.createNew);
 
 export const boardRoute = Router;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { APIs_V1 } from '~/routes/v1';
 
 const START_SERVER = () => {
     const app = express();
+    app.use(express.json());
 
     app.use('/v1', APIs_V1);
 
