dd2600706920dbf0bef91b08e673144bfb44fb0a
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-15 15:00:16 +0700

    Valid Model Apis

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import { ObjectId } from 'mongodb';
 import { GET_DB } from '~/config/mongodb';
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators';
 
@@ -15,9 +16,14 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
     _destroy: Joi.boolean().default(false),
 });
 
+const validateBeforeCreate = async (data) => {
+    return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false });
+};
+
 const createNew = async (data) => {
     try {
-        const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data);
+        const validData = await validateBeforeCreate(data);
+        const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData);
         return createdBoard;
     } catch (error) {
         throw new Error(error);
@@ -26,9 +32,11 @@ const createNew = async (data) => {
 
 const findOneById = async (id) => {
     try {
-        const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-            _id: id,
-        });
+        const result = await GET_DB()
+            .collection(BOARD_COLLECTION_NAME)
+            .findOne({
+                _id: new ObjectId(id),
+            });
         return result;
     } catch (error) {
         throw new Error(error);
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -11,10 +11,8 @@ const createNew = async (reqBody) => {
         };
 
         const createdBoard = await boardModel.createNew(newBoard);
-        console.log(createdBoard);
 
         const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
-        console.log(getNewBoard);
 
         // Trả kết quả về, trong Service luôn phải có return
         return getNewBoard;
