c4a75df88567ae1a8a7b58a5c1d81fcf1685bc07
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-15 12:11:18 +0700

    Created API

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,17 +1,43 @@
 import Joi from 'joi';
+import { GET_DB } from '~/config/mongodb';
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators';
 
 const BOARD_COLLECTION_NAME = 'boards';
 const BOARD_COLLECTION_SCHEMA = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict(),
     slug: Joi.string().required().min(3).trim().strict(),
     description: Joi.string().required().min(3).max(256).trim().strict(),
-    columnOrderIds: Joi.array().items(Joi.string()).default([]),
+
+    columnOrderIds: Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).default([]),
+
     createdAt: Joi.date().timestamp('javascript').default(Date.now()),
     updatedAt: Joi.date().timestamp('javascript').default(null),
     _destroy: Joi.boolean().default(false),
 });
 
+const createNew = async (data) => {
+    try {
+        const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data);
+        return createdBoard;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+const findOneById = async (id) => {
+    try {
+        const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+            _id: id,
+        });
+        return result;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
 export const boardModel = {
     BOARD_COLLECTION_NAME,
     BOARD_COLLECTION_SCHEMA,
+    createNew,
+    findOneById,
 };
