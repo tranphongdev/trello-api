c7df22449ae00b79d862c0f5f0fa5a01a16f74a9
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-15 16:23:39 +0700

    Handle Cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import exitHook from 'async-exit-hook';
 import { env } from '~/config/environment';
 
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
 import { APIs_V1 } from '~/routes/v1';
 import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware';
+import { corsOptions } from './config/cors';
 
 const START_SERVER = () => {
     const app = express();
+
+    // Xử lý CORS
+    app.use(cors(corsOptions));
+
     app.use(express.json());
 
     app.use('/v1', APIs_V1);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+// Những domain được truy cập tới server
+export const WHITELIST_DOMAINS = [
+    'http://localhost:5173',
+    // Sau này deloy lên domain chính thức ....
+];
