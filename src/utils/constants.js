c47bd3eae575c0ca2c71134e4bb8f423fb4bb667
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-17 12:36:44 +0700

    Update Whitelist Domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Những domain được truy cập tới server
 export const WHITELIST_DOMAINS = [
-    'https://trello-tranphongdev.vercel.app/',
+    'https://trello-tranphongdev.vercel.app',
     // 'http://localhost:5173',
     // Sau này deloy lên domain chính thức ....
 ];
