33f9b322d52aea933a26d0ed4c0b8e429128cf91
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-17 14:15:27 +0700

    TEST

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 // Những domain được truy cập tới server
 export const WHITELIST_DOMAINS = [
     'https://trello-tranphongdev.vercel.app',
-    'http://localhost:5173',
-    'https://trello-backend-yf5y.onrender.com',
+    // 'http://localhost:5173',
+    // 'https://trello-backend-yf5y.onrender.com',
     // Sau này deloy lên domain chính thức ....
 ];
 
